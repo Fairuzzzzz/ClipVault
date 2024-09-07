83a11c5b69eeda7e461a7722d64f0328c4affea4
fairuz <fairuz.afnan@gmail.com>
2024-09-07 21:49:37 +0700

    update ui and add fileserver

---
+++ ui/static/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll("nav a");
+    for (var i = 0; i < navLinks.length; i++) {
+        var link = navLinks[i];
+        if (link.getAttribute('href') == window.location.pathname) {
+            link.classList.add("live");
+            break;
+        }
+    }
+
+    const body = document.body;
+    const modeToggle = document.getElementById('mode-toggle');
+    const moonIcon = modeToggle.querySelector('.fa-moon');
+    const sunIcon = modeToggle.querySelector('.fa-sun');
+
+    function enableDarkMode() {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    }
+
+    function disableDarkMode() {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', null);
+    }
+
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        enableDarkMode();
+    }
+
+    modeToggle.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            disableDarkMode();
+        } else {
+            enableDarkMode();
+        }
+    });
+});
