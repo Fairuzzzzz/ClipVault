44515e429e78ca202d30a79dced0f13ac8194e8e
fairuz <fairuz.afnan@gmail.com>
2024-09-08 21:37:25 +0700

    configure new error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,19 +21,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.errorLog.Print(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func clipView(w http.ResponseWriter, r *http.Request) {
+func (app *application) clipView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil {
 		http.NotFound(w, r)
@@ -43,8 +42,8 @@ func clipView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific clip with ID %d...", id)
 }
 
-func clipCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+func (app *application) clipCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
 		return
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,55 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
+	// Command-line flag
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// Create a logger for writing information message
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error message
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize instance of application struct, containing the dependencies
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/clip/view", clipView)
-	mux.HandleFunc("/clip/create", clipCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/clip/view", app.clipView)
+	mux.HandleFunc("/clip/create", app.clipCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// Initialize a http.Server struct
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	// The value returned form flag.String() function is a pointer to the flag value
+	// not the value itself
+	infoLog.Printf("Starting server on %s", *addr) // Information message
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err) // Error message
 }
