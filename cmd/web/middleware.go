6c4b970b84a5594612c32ea27b2782925cb512bc
fairuz <fairuz.afnan@gmail.com>
2024-09-21 13:19:10 +0700

    add handlres and middleware test

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -11,9 +11,9 @@ import (
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().
-			Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src- fonts.google.com")
+			Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.google.com")
 
-		w.Header().Set("Referer-Policy", "origin-when-cross-origin")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
